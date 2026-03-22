1ccb8ed9e182cee5e2b62206a33962a7f53c6e54
romanzinchuk <romanzinchuk7@gmail.com>
2026-03-22 16:10:40 +0200

    feat(rcc): configure system clock to 100 MHz using HSI and PLL

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,24 @@
 #include "main.h"
+#include "stm32f4xx.h"
+
+void RCC_Init(void)
+{
+  // Enable HSI oscillator
+  RCC->CR |= RCC_CR_HSION; // CR - Clock Control Register
+  while (!(RCC->CR & RCC_CR_HSIRDY)); // Wait until HSI is ready
+
+  FLASH->ACR = FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS; // Configure Flash Access Control Register for 100 MHz
+  
+  // Configure PLL: M=8, N=100, P=2, Source=HSI to achieve 100 MHz system clock
+  RCC->PLLCFGR = (8 << RCC_PLLCFGR_PLLM_Pos) | 
+                 (100 << RCC_PLLCFGR_PLLN_Pos) |
+                 ( 0 << RCC_PLLCFGR_PLLP_Pos) |
+                 RCC_PLLCFGR_PLLSRC_HSI;
+
+  // Enable PLL
+  RCC->CR |= RCC_CR_PLLON;
+  while(!(RCC->CR & RCC_CR_PLLRDY)); // Wait until PLL is ready
+}
 
 int main(void)
 {
